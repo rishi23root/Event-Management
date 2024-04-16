48d32cc2a7fa9ad4df5f68ff0f2afabf4a24620c
Nishant4coding <srivastava4nishant@gmail.com>
2024-04-17 04:39:39 +0530

    ui updated

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -17,7 +17,7 @@ const NavItems = () => {
           <li
             key={link.route}
             className={`${
-              isActive && 'text-primary-500'
+              isActive && 'text-lime-600'
             } flex-center p-medium-16 whitespace-nowrap`}
           >
             <Link href={link.route}>{link.label}</Link>
